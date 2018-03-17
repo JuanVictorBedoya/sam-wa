0ab44a2ae33f35f905bc9c010ff0069a1472f7ea
Juan Victor <juanvictor.bedoya@gmail.com>
2018-03-17 01:05:12 -0400

    added user component with user menu options

--- src/client/components/navbar.jsx
+++ src/client/components/navbar.jsx
@@ -42,6 +42,96 @@ class NavBrand extends React.Component {
 	}
 }
 
+class NavUser extends React.Component {
+	constructor(props) {
+        super(props);
+        
+        this.status = {
+			preventDefault: true
+		};
+    }
+
+    componentDidMount() {
+		let uopt = this.refs.userOpt.classList;
+		window.addEventListener('click', ()=>{
+			if (uopt.contains('nav-user-dropdown-show') && this.status.preventDefault) {
+				uopt.remove('nav-user-dropdown-show');
+			}
+			this.status.preventDefault = true;
+		});
+	}
+
+	onClickSummary() {
+		this.status.preventDefault = false;
+		this.refs.userOpt.classList.toggle('nav-user-dropdown-show');
+    }
+    
+    render() {
+        return(
+            <li className="nav-right">
+                <div className="nav-user">
+                    <summary className="nav-user-wrapper" onClick={this.onClickSummary.bind(this)}>
+                    <img src={this.props.picture} className="nav-user-img"/>
+                        <span>
+                            <svg version="1.0" viewBox="0 0 8 8">
+                                <path d="M0 2l4 4 4-4H0z" ></path>
+                            </svg>
+                        </span>
+                    </summary>
+                    <div ref="userOpt" className="nav-user-dropdown arrow-up">
+						<div className="dropdown-boxarrow-up"></div>
+						<div className="nav-user-name">
+							<div style={{padding: '0rem 0rem 0.3rem 0rem'}}>Registrado como</div>
+                            <b>{this.props.user}</b>
+						</div>
+                        <hr className="nav-user-separator"/>
+						{this.props.children}
+					</div>
+                </div>
+            </li>
+        )
+    }
+}
+
+class NavUserLink extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<a className="nav-user-link">
+				<span>{this.props.text}</span>
+			</a>
+		);
+	}
+}
+
+class NavUserActionButton extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<a className="nav-user-link" onClick={this.props.onClick}>
+				<span>{this.props.text}</span>
+			</a>
+		);
+	}
+}
+
+class NavUserSeparator extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <hr className="nav-user-separator"/> 
+        );
+    }
+}
+
 class Navbar extends React.Component{
 	constructor(props){
         super(props);
@@ -57,4 +147,4 @@ class Navbar extends React.Component{
     }
 }
 
-export {Navbar, NavLink, NavBrand};
+export {Navbar, NavLink, NavBrand, NavUser, NavUserLink, NavUserSeparator, NavUserActionButton};
--- src/client/views/home.jsx
+++ src/client/views/home.jsx
@@ -11,7 +11,7 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { withApollo, graphql } from 'react-apollo';
 
-import {Navbar, NavLink, NavBrand} from '../components/navbar.jsx';
+import {Navbar, NavLink, NavBrand, NavUser, NavUserLink, NavUserSeparator, NavUserActionButton} from '../components/navbar.jsx';
 import {AppLogo_0} from '../components/app-logo.jsx';
 
 /****************************************************************************************/
@@ -21,15 +21,26 @@ class __Home__ extends React.Component {
 		super(props);
 	}
 
+	onLogoutClick() {
+	
+	}
+
 	render() {
+		
 		return (
 			<div>
 				<header>
-					<Navbar>
+					<Navbar >
 						<NavBrand logoComponent={AppLogo_0} logoStyle={{height: '20px', width: '20px', padding: '0.6rem'}}/>
 						<NavLink linkName="INICIO"/>
 						<NavLink linkName="PERFIL"/>
 						<NavLink linkName="DOCUMENTOS"/>
+						<NavUser user={'Juan Victor Bedoya'} picture={'/images/profile-pic.jpg'}>
+							<NavUserLink href="#home" text="Ver mi perfil"/>
+							<NavUserLink href="#about" text="Configuración"/>
+							<NavUserSeparator/>
+							<NavUserActionButton text="Salir"  onClick={this.onLogoutClick.bind(this)}/>
+						</NavUser>
 					</Navbar>
 				</header>
 				<main>
